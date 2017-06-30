a7d38470fa10471a0e6bfb265899b76c960d726e
James Collins <james@asone.co.uk>
2017-06-30 18:00:02 +0100

    Initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Timer App</li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/countdown" activeClassName="active-link"  activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">Created by <a href="https://github.com/jamescollins89" target="_blank">James Collins</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Navigation;
